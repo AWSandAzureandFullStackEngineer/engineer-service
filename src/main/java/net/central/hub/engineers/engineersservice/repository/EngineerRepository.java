0714de86b44d1f02a792241c71fc25c8cd766766
AWSandAzureandFullStackEngineer <awsandazurecloudengineer@gmail.com>
2024-01-30 15:44:22 -0600

    building.

--- src/main/java/net/central/hub/engineers/engineersservice/repository/EngineerRepository.java
+++
@@ -1,8 +0,0 @@
-package net.central.hub.engineers.engineersservice.repository;
-
-import net.central.hub.engineers.engineersservice.entity.Engineer;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.UUID;
-
-public interface EngineerRepository extends JpaRepository<Engineer, UUID> {
-}
